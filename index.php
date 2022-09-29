a887a9b632f0aace38fb927e2204df0bcd174ee2
John S. De Stefano Jr <deesto@yahoo.com>
2022-09-29 11:33:37 -0400

    Create index.php
    simple main page to include form

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- pull from ./food.php, which pulls from ./places -->
+<div>
+<p>Today, you should eat at:<br>
+<iframe src="food.php" marginwidth="0" marginheight="0" frameborder="0" height="30px" scrolling="no" width="650px"> </iframe></p>
+<form>
+<input type="button" value="Food fail? Try again!" onclick="history.go(0)" />
+</form>
+
+</div>
